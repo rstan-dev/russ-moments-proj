1ba405095bb0961bf674001defc4606329f611ae
Russ-S <rstanleym74@gmail.com>
2023-09-26 11:49:04 +0000

    create App.module.css and add basic site wide css, import to App.js, Create NavBar css module and import into Navbar.js, import google fonts into index.html

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,11 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/moments_logo.png'
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
   return (
-        <Navbar expand="md" fixed="top">
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
                 <Navbar.Brand><img src={logo} alt='moments logo' height='45' /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
