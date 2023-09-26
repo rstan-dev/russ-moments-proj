bca742df777a513ead46f3f0b146324d7326bf72
Russ-S <rstanleym74@gmail.com>
2023-09-26 15:48:09 +0000

    create signupform.js in pages/auth - add in boilplate code, update signup render function with imported SignUpForm in app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
 import {Route,Switch} from 'react-router-dom';
 import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
@@ -10,9 +11,9 @@ function App() {
       < NavBar />
       <Container className={styles.Main}>
         <Switch>
-            <Route exact path="/" render={() => <h1>Home Page</h1> }></Route>
-            <Route exact path="/signin" render={() => <h1>Sign in</h1> }></Route>
-            <Route exact path="/signup" render={() => <h1>Sign up</h1> }></Route>
+            <Route exact path="/" render={() => <h1>Home Page</h1> } />
+            <Route exact path="/signin" render={() => <h1>Sign in</h1> } />
+            <Route exact path="/signup" render={() => <SignUpForm /> } />
             <Route render={() => <p>Page not found!</p>} />
 
         </Switch>
