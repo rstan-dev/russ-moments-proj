47ec713c000e69cffc99114c49be2f3c5af874db
Russ-S <rstanleym74@gmail.com>
2023-09-26 15:08:00 +0000

    import NavLink to NavBar.js, wrap NavBar brand in a nav link and add to= route, change NavLinks, add classNames, Active classnames, update css, and exact prop to prevent Home icon staying active

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <Switch>
             <Route exact path="/" render={() => <h1>Home Page</h1> }></Route>
             <Route exact path="/signin" render={() => <h1>Sign in</h1> }></Route>
+            <Route exact path="/signup" render={() => <h1>Sign up</h1> }></Route>
         </Switch>
 
       </Container>
