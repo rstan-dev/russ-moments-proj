e5544c33af79c0797a6854b323252cb915cd5038
Russ-S <rstanleym74@gmail.com>
2023-09-27 16:50:45 +0000

    add currentuser context to navbar.js, add conditional for loggedInicons and loggedOuticons, and wrap logged out icons in an empty jsx element - to display certain icons when logged in or logged out

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,40 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/moments_logo.png'
 import styles from '../styles/NavBar.module.css'
 import { NavLink } from 'react-router-dom';
+import { CurrentUserContext } from '../App';
 
 const NavBar = () => {
-  return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
-            <Container>
-                <NavLink to="/">
-                    <Navbar.Brand><img src={logo} alt='moments logo' height='45' /></Navbar.Brand>
-                </NavLink>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" >
-                <Nav className="ml-auto text-left">
-                    <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
-                        <i className='fas fa-home'></i> Home</NavLink>
-                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
-                    <i className='fas fa-sign-in-alt'></i> Sign in</NavLink>
-                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
-                    <i className='fas fa-user-plus'></i> Sign up</NavLink>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-  )
+    const currentUser = useContext(CurrentUserContext)
+    const loggedInIcons = <>{currentUser?.username}</>
+    const loggedOutIcons = <>
+        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
+        <i className='fas fa-sign-in-alt'></i> Sign in</NavLink>
+        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
+        <i className='fas fa-user-plus'></i> Sign up</NavLink>
+        </>
+
+    return (
+            <Navbar className={styles.NavBar} expand="md" fixed="top">
+                <Container>
+                    <NavLink to="/">
+                        <Navbar.Brand><img src={logo} alt='moments logo' height='45' /></Navbar.Brand>
+                    </NavLink>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" >
+                    <Nav className="ml-auto text-left">
+                        <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
+                            <i className='fas fa-home'></i> Home</NavLink>
+                    {currentUser ? loggedInIcons : loggedOutIcons}
+
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+    )
 }
 
 export default NavBar
