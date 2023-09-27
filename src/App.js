b5422c6157d4b325f9df5b9acc88a46436738dc6
Russ-S <rstanleym74@gmail.com>
2023-09-27 16:37:18 +0000

    setup Current User and setCurrentUser contexts for export and to cover the navbar elements in app.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,47 @@ import {Route,Switch} from 'react-router-dom';
 import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const CurrentUserContext = createContext()
+export const setCurrentUserContext = createContext()
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+
+  const handleMount = async () => {
+    try {
+      const {data} = await axios.get('dj-rest-auth/user/')
+      setCurrentUser(data)
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    handleMount()
+
+  }, [])
+
   return (
-    <div className={styles.App}>
-      < NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-            <Route exact path="/" render={() => <h1>Home Page</h1> } />
-            <Route exact path="/signin" render={() => <SignInForm /> } />
-            <Route exact path="/signup" render={() => <SignUpForm /> } />
-            <Route render={() => <p>Page not found!</p>} />
+    <CurrentUserContext.Provider value={currentUser}>
+      <setCurrentUserContext.Provider value={setCurrentUser}>
+        <div className={styles.App}>
+          < NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+                <Route exact path="/" render={() => <h1>Home Page</h1> } />
+                <Route exact path="/signin" render={() => <SignInForm /> } />
+                <Route exact path="/signup" render={() => <SignUpForm /> } />
+                <Route render={() => <p>Page not found!</p>} />
 
-        </Switch>
+            </Switch>
 
-      </Container>
-    </div>
+          </Container>
+        </div>
+      </setCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
