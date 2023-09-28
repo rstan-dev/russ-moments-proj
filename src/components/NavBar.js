48171da4d67f81df7032aa177fecc94d463bd20b
Russ-S <rstanleym74@gmail.com>
2023-09-28 16:20:48 +0000

    add logout functionality to Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,23 @@ import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/moments_logo.png'
 import styles from '../styles/NavBar.module.css'
 import { NavLink } from 'react-router-dom';
-import { useCurrentUser } from '../contexts/CurrentUserContext';
+import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
+import Avatar from "./Avatar";
+import axios from 'axios';
 
 const NavBar = () => {
     const currentUser = useCurrentUser()
+    const setCurrentUser = useSetCurrentUser();
+
+    const handleSignOut = async () => {
+        try {
+            await axios.post('dj-rest-auth/logout/');
+            setCurrentUser(null);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
 
     const addPosticon = (
         <NavLink
@@ -39,7 +52,7 @@ const NavBar = () => {
         <NavLink
         className={styles.NavLink}
         to="/"
-        onClick={() => {}}
+        onClick={handleSignOut}
         >
         <i className='fas fa-sign-out-alt'></i> Sign Out
         </NavLink>
@@ -47,7 +60,7 @@ const NavBar = () => {
         <NavLink
         className={styles.NavLink}
         to={`/profiles/${currentUser?.profile_id}`}>
-        <img src={currentUser?.profile_image}/>
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
         </NavLink>
 
     </>
