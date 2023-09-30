0d68d84a3760b92aac691e8d491dd61ffa5b8bd9
Russ-S <rstanleym74@gmail.com>
2023-09-30 15:03:26 +0000

    add useEffect function and handlemount to call post data

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import {Route,Switch} from 'react-router-dom';
 import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
-import PostsPage from './pages/posts/PostsPage';
 import PostCreateForm from './pages/posts/PostsCreateForm';
+import PostPage from './pages/posts/PostPage';
+import PostsPage from './pages/posts/PostsPage';
 import { useCurrentUser } from './contexts/CurrentUserContext';
 
 
@@ -57,6 +58,7 @@ function App() {
                 <Route exact path="/signin" render={() => <SignInForm /> } />
                 <Route exact path="/signup" render={() => <SignUpForm /> } />
                 <Route exact path="/posts/create" render={() => <PostCreateForm /> } />
+                <Route exact path="/posts/:id" render={() => <PostPage /> } />
                 <Route render={() => <p>Page not found!</p>} />
             </Switch>
           </Container>
