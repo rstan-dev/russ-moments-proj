66ae9df0ed502adcbbadbdeac47704eee36cf942
Russ-S <rstanleym74@gmail.com>
2023-10-02 10:21:24 +0000

    create liked route and add likes filter code

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@ function App() {
 
             <Route exact path="/feed" render={() => <PostsPage message="No results found. Adjust the search keyword, or follow a user." filter={`owner__followed__owner__profile=${profile_id}&`} />} />
 
+            <Route exact path="/liked" render={() => <PostsPage message="No results found. Adjust the search keyword, or like a post." filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`} />} />
+
 
             <Route exact path="/signin" render={() => <SignInForm /> } />
             <Route exact path="/signup" render={() => <SignUpForm /> } />
