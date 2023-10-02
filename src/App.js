692e0c0adf64fefe142076b82e07d1f40fec5640
Russ-S <rstanleym74@gmail.com>
2023-10-02 10:14:47 +0000

    add boilerplate code (start over again here) to PostsPage.js. . Add Route to app.js

--- src/App.js
+++ src/App.js
@@ -22,39 +22,7 @@ function App() {
           < NavBar />
           <Container className={styles.Main}>
             <Switch>
-            <Route exact path="/" render={() => <h1>Home page</h1>} />
-                {/* <Route
-                  exact
-                  path="/"
-                  render={() => (
-                  <PostsPage
-                    message="No results found. Adjust the search keyword"
-                  />
-                  )}
-                />
-
-                <Route
-                  exact
-                  path="/feed"
-                  render={() => (
-                  <PostsPage
-                    message="No results found. Adjust the search keyword or follow a user"
-                    filter={`owner__followed__owner__profile=${profile_id}&`}
-                    />
-                  )}
-                />
-
-                <Route
-                  exact
-                  path="/liked"
-                  render={() => (
-                  <PostsPage
-                  message="No results found. Adjust the search keyword or like a post"
-                  filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
-                  />
-                  )}
-                /> */}
-
+            <Route exact path="/" render={() => <PostsPage message="No results found, Adjust the search keyword." />} />
                 <Route exact path="/signin" render={() => <SignInForm /> } />
                 <Route exact path="/signup" render={() => <SignUpForm /> } />
                 <Route exact path="/posts/create" render={() => <PostCreateForm /> } />
