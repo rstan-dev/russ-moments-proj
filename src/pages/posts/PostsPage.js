9719eae401f2077147be9a0f880b09aa4ee2256f
Russ-S <rstanleym74@gmail.com>
2023-10-02 10:41:55 +0000

    create variables for useState for posts, hasLoaded and useLocation. . Create useEffect hook to fetchPosts

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-
+import { useLocation } from "react-router"
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
+import { axiosReq } from "../../api/axiosDefaults";
 
-function PostsPage() {
+function PostsPage({message, filter = ""}) {
+    const [posts, setPosts] = useState({ results: []});
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const { pathname } = useLocation();
 
-  return (
-    <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
-      </Col>
-      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
-      </Col>
-    </Row>
-  );
-}
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const {data} = await axiosReq.get(`/posts/?${filter}`);
+                setPosts(data);
+                setHasLoaded(true);
+            } catch(err) {
+                console.log(err);
+            }
+        };
+        setHasLoaded(false);
+        fetchPosts();
+    }, [filter, pathname]);
+
+    return (
+        <Row className="h-100">
+        <Col className="py-2 p-0 p-lg-2" lg={8}>
+            <p>Popular profiles mobile</p>
+            <p>List of posts here</p>
+        </Col>
+        <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+            <p>Popular profiles for desktop</p>
+        </Col>
+        </Row>
+    );
+    }
 
 export default PostsPage;
